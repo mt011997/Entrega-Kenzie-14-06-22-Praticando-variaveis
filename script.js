da2019e323932c02e3b0df67766493cf58ce7a63
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2022-12-28 12:58:45 -0300

    Complete

---
+++ script.js
@@ -0,0 +1,24 @@
+let nome = "Matheus"
+let sobrenome = "Oliveira"
+let anoNascimento = 1997
+let cidade = "Rio de Janeiro"
+let idade = 2022 - anoNascimento
+
+console.log(`Eu sou o/a ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}`)
+
+
+
+/* Teste com texto interativo ao usuário */
+
+
+/*let nome = prompt("Digite seu nome:")
+let sobrenome =  prompt("Digite seu sobrenome:")
+let anoNascimento =  prompt("Digite ano do nascimento:")
+let cidade =  prompt("Digite sua cidade:")
+let idade = 2022 - anoNascimento
+
+alert(`Eu sou o/a ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}`)*/
+
+for (let i = 0; i <= 5; i++) {
+    console.log(i)
+}
